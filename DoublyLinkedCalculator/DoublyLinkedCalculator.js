c2b1d32c20cbbb9e401299fcf201a9c03f735a84
melnee <Melanie.Neilan@gmail.com>
2020-11-25 15:35:49 -0800

    Create DoublyLinkedCalculator.js
    Making a calculator app that can take this type of input:
    "1+2+6*5/4-9" and output the result via pemdas. Used a doubly linked list to do it.  :D

---
+++ DoublyLinkedCalculator/DoublyLinkedCalculator.js
@@ -0,0 +1,13 @@
+class DoublyLinkedList {
+    constructor(head = null){
+        this.head = head;
+    }
+}
+
+ class ListNode{
+     constructor(value){
+        this.value = value;
+     }
+
+    
+ }
