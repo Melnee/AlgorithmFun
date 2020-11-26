8831b06a8bbe7ea480308a94aff61fae54b59522
melnee <Melanie.Neilan@gmail.com>
2020-11-25 18:34:43 -0800

    Update DoublyLinkedCalculator.js
    Sucessfully took test input strink and divided characters into a doubly linked list, then printed values

--- DoublyLinkedCalculator/DoublyLinkedCalculator.js
+++ DoublyLinkedCalculator/DoublyLinkedCalculator.js
@@ -4,8 +4,8 @@
 // ██║     ██║╚════██║   ██║       ██║     ██║     ██╔══██║╚════██║╚════██║
 // ███████╗██║███████║   ██║       ╚██████╗███████╗██║  ██║███████║███████║
 // ╚══════╝╚═╝╚══════╝   ╚═╝        ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝
-                                                                        
-                            
+
+                   
 class DoublyLinkedList{
     constructor(head = null){
         this.head = head;
@@ -29,14 +29,45 @@ class DoublyLinkedList{
 
 
 
-// ████████╗███████╗███████╗████████╗
-// ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝
-//    ██║   █████╗  ███████╗   ██║   
-//    ██║   ██╔══╝  ╚════██║   ██║   
-//    ██║   ███████╗███████║   ██║   
-//    ╚═╝   ╚══════╝╚══════╝   ╚═╝   
-                                  
+
+
+
+//  ██╗   ██╗███████╗███████╗██████╗     ██╗███╗   ██╗██████╗ ██╗   ██╗████████╗
+//  ██║   ██║██╔════╝██╔════╝██╔══██╗    ██║████╗  ██║██╔══██╗██║   ██║╚══██╔══╝
+//  ██║   ██║███████╗█████╗  ██████╔╝    ██║██╔██╗ ██║██████╔╝██║   ██║   ██║   
+//  ██║   ██║╚════██║██╔══╝  ██╔══██╗    ██║██║╚██╗██║██╔═══╝ ██║   ██║   ██║   
+//  ╚██████╔╝███████║███████╗██║  ██║    ██║██║ ╚████║██║     ╚██████╔╝   ██║   
+//   ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝    ╚═╝   
+
 var newList = new DoublyLinkedList();
-var newNode = new Node("3");
-newList.head = newNode;
-console.log(newList.head.value);
+
+var testInput = "1+3+4+6+7";
+var symbols = ["+", "-", "/", "*"];
+
+for(var i = 0; i < testInput.length; i++){
+    //create a new node to store the character
+    var newNode = new Node(testInput[i]);
+    //check if the list is empty
+    if(newList.head == (null)){
+        newList.head = newNode;
+    //if it's not empty, place the new node at the end, using a runner
+    }else{
+        var runner = newList.head;
+        while(runner.next != null){
+            runner = runner.next;
+        }
+        runner.next = newNode;
+    }
+}
+
+
+//to test that it worked, create a runner and print every value in the list
+
+var runner = newList.head;
+console.log(runner.value)
+while(runner.next != null){
+    console.log(runner.next.value);
+    runner = runner.next;
+}
+
+//it worked!!! :D
